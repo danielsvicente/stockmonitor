11b6bb60d2ae77526b2d7c9de2a59883ff6e838d
danielsv <daniel_vs@msn.com>
2018-09-18 22:41:42 +0100

    Update stock shares and add header infos on intraday frame

--- stockmonitor.py
+++ stockmonitor.py
@@ -11,22 +11,22 @@ plt.style.use('dark_background')
 plt.figure().subplots_adjust(hspace=0.5)
 plt.ion()
 mng = plt.get_current_fig_manager()
-mng.full_screen_toggle()
+#mng.full_screen_toggle()
 
 abev_stock = 50
 movi_stock = 300
 pomo_stock = 100
-qgep_stock = 100
-wege_stock = 100
+goau_stock = 100
+wege_stock = 130
 
 abev_buy_average = (980.00) / 50
 movi_buy_average = (848.00 + 2198.00) / 300
 pomo_buy_average = (401.00) / 100
-qgep_buy_average = (1086.00) / 100
+goau_buy_average = (975.00) / 100
 wege_buy_average = (1989.00) / 100
 
 investment_value = 7300.00
-remaining_balance = 154.22
+remaining_balance = 373.01
 
 days = 30
 start = date.today() - timedelta(days=days)
@@ -52,30 +52,30 @@ while True:
 		movi = web.get_data_yahoo('MOVI3.SA', start, end)
 		print("Pulling POMO4")
 		pomo = web.get_data_yahoo('POMO4.SA', start, end)
-		print("Pulling QGEP3")
-		qgep = web.get_data_yahoo('QGEP3.SA', start, end)
+		print("Pulling GOAU4")
+		goau = web.get_data_yahoo('GOAU4.SA', start, end)
 		print("Pulling WEGE3")
 		wege = web.get_data_yahoo('WEGE3.SA', start, end)
 
 		abev['Diff'] = abev['Close'].diff()
 		movi['Diff'] = movi['Close'].diff()
 		pomo['Diff'] = pomo['Close'].diff()
-		qgep['Diff'] = qgep['Close'].diff()
+		goau['Diff'] = goau['Close'].diff()
 		wege['Diff'] = wege['Close'].diff()
 
 		abev['Total'] = abev['Close'] * abev_stock
 		movi['Total'] = movi['Close'] * movi_stock
 		pomo['Total'] = pomo['Close'] * pomo_stock
-		qgep['Total'] = qgep['Close'] * qgep_stock
+		goau['Total'] = goau['Close'] * goau_stock
 		wege['Total'] = wege['Close'] * wege_stock
 
-		historical = abev['Total'] + movi['Total'] + pomo['Total'] + qgep['Total'] + wege['Total']
+		historical = abev['Total'] + movi['Total'] + pomo['Total'] + goau['Total'] + wege['Total']
 		
 		'''
 		abev['Diff'] = pd.Series(abev['Close'] - abev_buy_average, index=abev.index)
 		movi['Diff'] = pd.Series(movi['Close'] - movi_buy_average, index=movi.index)
 		pomo['Diff'] = pd.Series(pomo['Close'] - pomo_buy_average, index=pomo.index)
-		qgep['Diff'] = pd.Series(qgep['Close'] - qgep_buy_average, index=qgep.index)
+		goau['Diff'] = pd.Series(goau['Close'] - goau_buy_average, index=goau.index)
 		wege['Diff'] = pd.Series(wege['Close'] - wege_buy_average, index=wege.index)
 		'''
 
@@ -83,23 +83,23 @@ while True:
 		abev.fillna(method='ffill', inplace=True)
 		movi.fillna(method='ffill', inplace=True)
 		pomo.fillna(method='ffill', inplace=True)
-		qgep.fillna(method='ffill', inplace=True)
+		goau.fillna(method='ffill', inplace=True)
 		wege.fillna(method='ffill', inplace=True)
 
-		qgep_current_rate = qgep.iloc[-1]['Close']
+		goau_current_rate = goau.iloc[-1]['Close']
 		pomo_current_rate = pomo.iloc[-1]['Close']
 		wege_current_rate = wege.iloc[-1]['Close']
 		movi_current_rate = movi.iloc[-1]['Close']
 		abev_current_rate = abev.iloc[-1]['Close']
 
-		qgep_previous_close = qgep.iloc[-2]['Close']
+		goau_previous_close = goau.iloc[-2]['Close']
 		pomo_previous_close = pomo.iloc[-2]['Close']
 		wege_previous_close = wege.iloc[-2]['Close']
 		movi_previous_close = movi.iloc[-2]['Close']
 		abev_previous_close = abev.iloc[-2]['Close']
 
 		# EACH SHARE
-		qgep_change, qgep_change_percentage = calc_change(qgep_previous_close, qgep_current_rate)
+		goau_change, goau_change_percentage = calc_change(goau_previous_close, goau_current_rate)
 		pomo_change, pomo_change_percentage = calc_change(pomo_previous_close, pomo_current_rate)
 		wege_change, wege_change_percentage = calc_change(wege_previous_close, wege_current_rate)
 		movi_change, movi_change_percentage = calc_change(movi_previous_close, movi_current_rate)
@@ -107,16 +107,21 @@ while True:
 		###
 
 		# OVERALL
-		current_shares_value = (qgep_current_rate * qgep_stock) + (pomo_current_rate * pomo_stock) + (wege_current_rate * wege_stock) + (movi_current_rate * movi_stock) + (abev_current_rate * abev_stock)
+		current_shares_value = (goau_current_rate * goau_stock) + (pomo_current_rate * pomo_stock) + (wege_current_rate * wege_stock) + (movi_current_rate * movi_stock) + (abev_current_rate * abev_stock)
 		current_account_value = current_shares_value + remaining_balance
 		current_yield_value = current_account_value - investment_value
 		current_yield_percentage = (current_account_value * 100 / investment_value) - 100
+
+		last_shares_value = (goau_previous_close * goau_stock) + (pomo_previous_close * pomo_stock) + (wege_previous_close * wege_stock) + (movi_previous_close * movi_stock) + (abev_previous_close * abev_stock)
+		current_day_yield_value = current_shares_value - last_shares_value
+		current_day_yield_percentage = (current_shares_value * 100 / last_shares_value) - 100
+		
 		###
 
 		print('ABEV3 : ' + format_value(abev_current_rate) + ' ' + format_value(abev_change) + ' (' + format_value(abev_change_percentage) + ')' )
 		print('MOVI3 : ' + format_value(movi_current_rate) + ' ' + format_value(movi_change) + ' (' + format_value(movi_change_percentage) + ')' )
 		print('POMO4 : ' + format_value(pomo_current_rate) + ' ' + format_value(pomo_change) + ' (' + format_value(pomo_change_percentage) + ')' )
-		print('QGEP3 : ' + format_value(qgep_current_rate) + ' ' + format_value(qgep_change) + ' (' + format_value(qgep_change_percentage) + ')' )
+		print('GOAU4 : ' + format_value(goau_current_rate) + ' ' + format_value(goau_change) + ' (' + format_value(goau_change_percentage) + ')' )
 		print('WEGE3 : ' + format_value(wege_current_rate) + ' ' + format_value(wege_change) + ' (' + format_value(wege_change_percentage) + ')' )
 		print('SHARES TOTAL : ' + format_value(current_shares_value))
 
@@ -139,8 +144,8 @@ while True:
 		pomo['Close'].plot(color='cyan')
 
 		plt.subplot2grid((3, 5), (0, 3), rowspan=1, colspan=1)
-		plt.title('QGEP3 : ' + format_value(qgep_current_rate) + ' ' + format_value(qgep_change) + ' (' + format_value(qgep_change_percentage) + '%)' )
-		qgep['Close'].plot(color='cyan')
+		plt.title('GOAU4 : ' + format_value(goau_current_rate) + ' ' + format_value(goau_change) + ' (' + format_value(goau_change_percentage) + '%)' )
+		goau['Close'].plot(color='cyan')
 
 		plt.subplot2grid((3, 5), (0, 4), rowspan=1, colspan=1)
 		plt.title('WEGE3 : ' + format_value(wege_current_rate) + ' ' + format_value(wege_change) + ' (' + format_value(wege_change_percentage) + '%)' )
@@ -166,7 +171,7 @@ while True:
 
 		plt.subplot2grid((3, 5), (1, 3), rowspan=1, colspan=1)
 		plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
-		qgep['Diff'].plot(kind='bar', color='cyan')
+		goau['Diff'].plot(kind='bar', color='cyan')
 
 		plt.subplot2grid((3, 5), (1, 4), rowspan=1, colspan=1)
 		plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
@@ -175,11 +180,12 @@ while True:
 		# Row 3 - Historical and Intraday
 		hist_series = pd.Series(historical)
 		plt.subplot2grid((3, 5), (2, 0), rowspan=1, colspan=2)
-		plt.title('No dia : ' + format_value(current_account_value) + ' ' + format_value(current_yield_value) + ' (' + format_value(current_yield_percentage) + '%)' )
+		plt.title('Total : ' + format_value(current_account_value) + ' ' + format_value(current_yield_value) + ' (' + format_value(current_yield_percentage) + '%)' )
 		hist_series.plot(color='cyan')		
 
 		intraday_series = pd.Series(intraday)
 		plt.subplot2grid((3, 5), (2, 2), rowspan=1, colspan=3)
+		plt.title('No dia : ' + format_value(current_day_yield_value) + ' (' + format_value(current_day_yield_percentage) + '%)' )
 		intraday_series.plot(color='cyan')
 		
 		plt.pause(0.05)
